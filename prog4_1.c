aa6448645fef930a20ed7f0a3bdeed15ff6a7643
Taylor Ewertz <t_ewertz@yahoo.com>
2016-11-04 15:09:47 -0700

    prog4_1.c

---
+++ prog4_1.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+    #include <string.h>
+    #include <lua.h>
+    #include <lauxlib.h>
+    #include <lualib.h>
+    
+    int main (void) {
+      printf("Assignment #4-1,Taylor Ewertz, t_ewertz@yahoo.com\n");
+      char buff[256];
+      int error;
+      lua_State *L =  luaL_newstate();  /* opens Lua */
+      luaopen_base(L);             /* opens the basic library */
+      luaopen_table(L);            /* opens the table library */
+      luaopen_io(L);               /* opens the I/O library */
+      luaopen_string(L);           /* opens the string lib. */
+      luaopen_math(L);             /* opens the math lib. */
+      luaL_dofile(L, "test.lua");
+    
+      while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        error = luaL_loadbuffer(L, buff, strlen(buff), "line") ||
+                lua_pcall(L, 0, 0, 0);
+        if (error) {
+          fprintf(stderr, "%s", lua_tostring(L, -1));
+          lua_pop(L, 1);  /* pop error message from the stack */
+        }
+      }
+    
+      lua_close(L);
+      return 0;
+    }
