696bc73c59a2c08dd6c95b8a032c0507b129d6b6
Taylor Ewertz <t_ewertz@yahoo.com>
2016-11-08 14:54:37 -0800

    merge with repo

--- prog4_1.c
+++ prog4_1.c
@@ -1,11 +1,11 @@
 
 #include <stdio.h>
-    #include <string.h>
-    #include <lua.h>
-    #include <lauxlib.h>
-    #include <lualib.h>
+#include <string.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
     
-    int main (int argc, char **argv) {
+int main (int argc, char **argv) {
       printf("Assignment #4-1,Taylor Ewertz, t_ewertz@yahoo.com\n");
       char buff[256];
       int error;
@@ -16,16 +16,7 @@
       luaopen_string(L);           /* opens the string lib. */
       luaopen_math(L);             /* opens the math lib. */
       luaL_dofile(L, argv[1]);
+      lua_close(L);    
     
-      while (fgets(buff, sizeof(buff), stdin) != NULL) {
-        error = luaL_loadbuffer(L, buff, strlen(buff), "line") ||
-                lua_pcall(L, 0, 0, 0);
-        if (error) {
-          fprintf(stderr, "%s", lua_tostring(L, -1));
-          lua_pop(L, 1);  /* pop error message from the stack */
-        }
-      }
-    
-      lua_close(L);
       return 0;
-    }
+}
