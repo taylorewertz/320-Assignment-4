44b2bb1493183145b015a9e6bffdce9006587d3c
Taylor Ewertz <t_ewertz@yahoo.com>
2016-11-08 15:25:21 -0800

    got rid of unnecessary stuff in intrepeter

--- prog4_1.c
+++ prog4_1.c
@@ -7,14 +7,9 @@
     
 int main (int argc, char **argv) {
       printf("Assignment #4-1,Taylor Ewertz, t_ewertz@yahoo.com\n");
-      char buff[256];
-      int error;
+
       lua_State *L =  luaL_newstate();  /* opens Lua */
-      luaopen_base(L);             /* opens the basic library */
-      luaopen_table(L);            /* opens the table library */
-      luaopen_io(L);               /* opens the I/O library */
-      luaopen_string(L);           /* opens the string lib. */
-      luaopen_math(L);             /* opens the math lib. */
+      luaL_openlibs(L);
       luaL_dofile(L, argv[1]);
       lua_close(L);    
     
